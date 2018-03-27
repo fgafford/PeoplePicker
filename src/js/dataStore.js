0f1160a68f7092dcb040fcf71a9a574cc9be2ae8
fgafford <forest.gafford@gmail.com>
2018-03-26 23:22:14 -0400

    Auth to Google Drive working

--- src/js/dataStore.js
+++ src/js/dataStore.js
@@ -1,17 +1,7 @@
-// Enter an API key from the Google API Console:
-//   https://console.developers.google.com/apis/credentials
-const apiKey = 'YOUR_API_KEY';
-
 // Enter the API Discovery Docs that describes the APIs you want to
 // access. In this example, we are accessing the People API, so we load
 // Discovery Doc found here: https://developers.google.com/people/api/rest/
-const discoveryDocs = ["https://people.googleapis.com/$discovery/rest?version=v1"];
-
-// Enter a client ID for a web application from the Google API Console:
-//   https://console.developers.google.com/apis/credentials?project=_
-// In your API Console project, add a JavaScript origin that corresponds
-//   to the domain where you will be running the script.
-const clientId = 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com';
+const discoveryDocs = ['https://people.googleapis.com/$discovery/rest'];
 
 // Enter one or more authorization scopes. Refer to the documentation for
 // the API or https://developers.google.com/people/v1/how-tos/authorizing
@@ -20,16 +10,34 @@ const scopes = 'profile';
 var authorizeButton = document.getElementById('authorize-button');
 var signoutButton = document.getElementById('signout-button');
 
-function handleClientLoad() {
+function handleClientLoad() {  
+  // Request data from user if not already found in local storage
+  if(!localStorage.clientId){
+    localStorage.clientId = prompt("Please enter client_id:", "?????????????.apps.googleusercontent.com")
+  }
+  if(!localStorage.apiKey){
+    localStorage.apiKey = prompt("Please enter API key:", "?????????????_?????_???????_?????????")
+  }
+
+  // Enter an API key from the Google API Console:
+  //   https://console.developers.google.com/apis/credentials
+  // const apiKey = localStorage.apiKey;
+  
+  // Enter a client ID for a web application from the Google API Console:
+  //   https://console.developers.google.com/apis/credentials?project=_
+  // In your API Console project, add a JavaScript origin that corresponds
+  //   to the domain where you will be running the script.
+  // const clientId = localStorage.clientId;
+
   // Load the API client and auth2 library
   gapi.load('client:auth2', initClient);
 }
 
 function initClient() {
   gapi.client.init({
-      apiKey: apiKey,
+      apiKey: localStorage.apiKey,
       discoveryDocs: discoveryDocs,
-      clientId: clientId,
+      clientId: localStorage.clientId,
       scope: scopes
   }).then(function () {
     // Listen for sign-in state changes.
@@ -38,7 +46,8 @@ function initClient() {
     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
     authorizeButton.onclick = handleAuthClick;
     signoutButton.onclick = handleSignoutClick;
-  });
+  })
+  .catch(err => console.log(err));
 }
 
 function updateSigninStatus(isSignedIn) {
